98bd3beb921fa9eb2a06ea5c864994bb8eff0c17
Carlos Duque <carlos.duquerivera.0308@gmail.com>
2021-03-02 19:09:53 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const inputButton = document.getElementById("input-btn");
+const clearButton = document. getElementById("clear-btn");
+const counter = document.getElementById("counter-count");
+const input = document.getElementById("input-bar");
+
+
+inputButton.addEventListener("click", () => {
+       
+    let numOfWords;
+    if (input.value === "") {
+        numOfWords = 0;
+        counter.innerHTML = numOfWords;
+    }
+    else {
+        let spaceCounter = 0;
+        for (let i = 0; i < input.value.length; i++) {
+            if (input.value[i] === " ") {
+                spaceCounter++
+            }  
+        };  
+        numOfWords = spaceCounter + 1;
+        counter.innerHTML = numOfWords;
+    } 
+});
+
+clearButton.addEventListener("click", () => {
+    input.value = "";
+    counter.innerHTML = 0;
+})
+
+
